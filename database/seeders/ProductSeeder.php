10561a5bbfba3cf26a2e6e7f9fd15a3ac595c5de
isanra <ihsanr293@gmail.com>
2026-01-16 09:47:19 +0700

    apdettt

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -15,17 +15,12 @@ class ProductSeeder extends Seeder
         $items = [
             // Kategori: Main Course
             [
-                'name' => 'Crazy Rich Rice Bowl', 
-                'price' => 35000, 
-                'image' => 'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?w=500&q=80', 
+                'name' => 'Japlak', 
+                'price' => 27000, 
+                'image' => '/img/japlak.png', 
                 'category' => 'Main Course'
             ],
-            [
-                'name' => 'Mega Steak & Fries', 
-                'price' => 85000, 
-                'image' => 'https://images.unsplash.com/photo-1600891964092-4316c288032e?w=500&q=80', 
-                'category' => 'Main Course'
-            ],
+            
 
             // Kategori: Drinks
             [
@@ -54,12 +49,7 @@ class ProductSeeder extends Seeder
                 'image' => '/img/bun.png', 
                 'category' => 'Bread'
             ],
-            [
-                'name' => 'Croissant Butter', 
-                'price' => 18000, 
-                'image' => 'https://images.unsplash.com/photo-1555507036-ab1f4038808a?w=500&q=80', 
-                'category' => 'Bread'
-            ],
+            
 
             // Kategori: Fruiti
             [
@@ -68,12 +58,7 @@ class ProductSeeder extends Seeder
                 'image' => '/img/fruiti.png', 
                 'category' => 'Fruiti'
             ],
-            [
-                'name' => 'Summer Fruit Salad', 
-                'price' => 32000, 
-                'image' => 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?w=500&q=80', 
-                'category' => 'Fruiti'
-            ],
+            
         ];
 
         foreach ($items as $item) {
